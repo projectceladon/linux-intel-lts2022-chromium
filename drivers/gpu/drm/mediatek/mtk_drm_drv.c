947cf52bf6c2a9409a43259a43639d35c35f1967
CK Hu <ck.hu@mediatek.com>
2016-01-12 16:15:50 +0100

    CHROMIUM: drm/mediatek: Add interface to allocate Mediatek GEM buffer.
    Add an interface to allocate Mediatek GEM buffers, allow the IOCTLs
    to be used by render nodes.
    This patch also sets the RENDER driver feature.
    
    Signed-off-by: CK Hu <ck.hu@mediatek.com>
    Signed-off-by: Nicolas Boichat <drinkcat@chromium.org>
    Signed-off-by: Philipp Zabel <p.zabel@pengutronix.de>
    
    BUG=chromium:1021640
    BUG=b:109911488
    TEST=build and boot kukui to UI
    
    Reviewed-on: https://chromium-review.googlesource.com/327581
    Commit-Ready: Daniel Kurtz <djkurtz@chromium.org>
    Tested-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-by: Daniel Kurtz <djkurtz@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/423687
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/1186281
    Commit-Ready: Nicolas Boichat <drinkcat@chromium.org>
    Tested-by: Pi-Hsun Shih <pihsun@chromium.org>
    Conflicts:
    	drivers/gpu/drm/mediatek/mtk_drm_drv.c
    	drivers/gpu/drm/mediatek/mtk_drm_gem.c
    
    [rebase54(groeck):
        Fixed conflicts:
    	- drm_gem_object_unreference_unlocked -> drm_gem_object_put_unlocked
    	- Added missing include files
    	- Addressed context conflicts
        Squashed:
    	FIXUP: CHROMIUM: drm/mediatek: Add interface to allocate Mediatek GEM buffer
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    [rebase510(eizan)
        Fixed conflicts:
    	- drm_gem_object_put_unlocked -> drm_gem_object_put
    ]
    Signed-off-by: Eizan Miyamoto <groeck@chromium.org>
    
    Change-Id: Ice3ed33b3f41e5c02c39d77a8f9e6775448a41bb
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/1955361
    Tested-by: Nicolas Boichat <drinkcat@chromium.org>
    Commit-Queue: Nicolas Boichat <drinkcat@chromium.org>
    Reviewed-by: Nicolas Boichat <drinkcat@chromium.org>

--- drivers/gpu/drm/mediatek/mtk_drm_drv.c
+++ drivers/gpu/drm/mediatek/mtk_drm_drv.c
@@ -26,6 +26,8 @@
 #include <drm/drm_probe_helper.h>
 #include <drm/drm_vblank.h>
 
+#include <drm/mediatek_drm.h>
+
 #include "mtk_drm_crtc.h"
 #include "mtk_drm_ddp_comp.h"
 #include "mtk_drm_drv.h"
@@ -453,6 +455,14 @@ static void mtk_drm_kms_deinit(struct drm_device *drm)
 	component_unbind_all(drm->dev, drm);
 }
 
+static const struct drm_ioctl_desc mtk_ioctls[] = {
+	DRM_IOCTL_DEF_DRV(MTK_GEM_CREATE, mtk_gem_create_ioctl,
+			  DRM_UNLOCKED | DRM_AUTH | DRM_RENDER_ALLOW),
+	DRM_IOCTL_DEF_DRV(MTK_GEM_MAP_OFFSET,
+			  mtk_gem_map_offset_ioctl,
+			  DRM_UNLOCKED | DRM_AUTH | DRM_RENDER_ALLOW),
+};
+
 DEFINE_DRM_GEM_FOPS(mtk_drm_fops);
 
 /*
@@ -468,7 +478,8 @@ static struct drm_gem_object *mtk_drm_gem_prime_import(struct drm_device *dev,
 }
 
 static const struct drm_driver mtk_drm_driver = {
-	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC,
+	.driver_features = DRIVER_MODESET | DRIVER_GEM | DRIVER_ATOMIC |
+			   DRIVER_RENDER,
 
 	.dumb_create = mtk_drm_gem_dumb_create,
 
@@ -477,6 +488,10 @@ static const struct drm_driver mtk_drm_driver = {
 	.gem_prime_import = mtk_drm_gem_prime_import,
 	.gem_prime_import_sg_table = mtk_gem_prime_import_sg_table,
 	.gem_prime_mmap = drm_gem_prime_mmap,
+
+	.ioctls = mtk_ioctls,
+	.num_ioctls = ARRAY_SIZE(mtk_ioctls),
+
 	.fops = &mtk_drm_fops,
 
 	.name = DRIVER_NAME,
