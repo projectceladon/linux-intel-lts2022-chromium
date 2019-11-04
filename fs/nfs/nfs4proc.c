85634c066fc388072622fa1da30c3120a7b5cd8d
Mark Salyzyn <salyzyn@google.com>
2019-11-04 08:57:10 -0800

    FROMLIST: Add flags option to get xattr method paired to __vfs_getxattr
    Add a flag option to get xattr method that could have a bit flag of
    XATTR_NOSECURITY passed to it.  XATTR_NOSECURITY is generally then
    set in the __vfs_getxattr path when called by security
    infrastructure.
    
    This handles the case of a union filesystem driver that is being
    requested by the security layer to report back the xattr data.
    
    For the use case where access is to be blocked by the security layer.
    
    The path then could be security(dentry) ->
    __vfs_getxattr(dentry...XATTR_NOSECURITY) ->
    handler->get(dentry...XATTR_NOSECURITY) ->
    __vfs_getxattr(lower_dentry...XATTR_NOSECURITY) ->
    lower_handler->get(lower_dentry...XATTR_NOSECURITY)
    which would report back through the chain data and success as
    expected, the logging security layer at the top would have the
    data to determine the access permissions and report back the target
    context that was blocked.
    
    Without the get handler flag, the path on a union filesystem would be
    the errant security(dentry) -> __vfs_getxattr(dentry) ->
    handler->get(dentry) -> vfs_getxattr(lower_dentry) -> nested ->
    security(lower_dentry, log off) -> lower_handler->get(lower_dentry)
    which would report back through the chain no data, and -EACCES.
    
    For selinux for both cases, this would translate to a correctly
    determined blocked access. In the first case with this change a correct avc
    log would be reported, in the second legacy case an incorrect avc log
    would be reported against an uninitialized u:object_r:unlabeled:s0
    context making the logs cosmetically useless for audit2allow.
    
    This patch series is inert and is the wide-spread addition of the
    flags option for xattr functions, and a replacement of __vfs_getxattr
    with __vfs_getxattr(...XATTR_NOSECURITY).
    
    Signed-off-by: Mark Salyzyn <salyzyn@android.com>
    Reviewed-by: Jan Kara <jack@suse.cz>
    Acked-by: Jan Kara <jack@suse.cz>
    Acked-by: Jeff Layton <jlayton@kernel.org>
    Acked-by: David Sterba <dsterba@suse.com>
    Acked-by: Darrick J. Wong <darrick.wong@oracle.com>
    Acked-by: Mike Marshall <hubcap@omnibond.com>
    Cc: Stephen Smalley <sds@tycho.nsa.gov>
    Cc: linux-kernel@vger.kernel.org
    Cc: kernel-team@android.com
    Cc: linux-security-module@vger.kernel.org
    
    (cherry picked from (rejected from archive because of too many recipients))
    Signed-off-by: Mark Salyzyn <salyzyn@google.com>
    Bug: 133515582
    Bug: 136124883
    Bug: 129319403
    Change-Id: Iabbb8771939d5f66667a26bb23ddf4c562c349a1
    
    Conflicts:
    	security/integrity/evm/evm_main.c
    [rebase510(groeck): Context conflicts]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    [rebase515(groeck):
      Squashed:
        FROMLIST: Add flags option to get xattr method paired to __vfs_getxattr
        FIXUP: FROMLIST: Add flags option to get xattr method paired to __vfs_getxattr
        FIXUP: ima: Define new template fields xattrnames, xattrlengths and xattrvalues
    ]
    
    Signed-off-by: Guenter Roeck <groeck@google.com>
    
    [rebase61(tzungbi):
        Squashed:
    	FIXUP: NFSv4.1: Enable access to the NFSv4.1 'dacl' and 'sacl' attributes
    ]
    Signed-off-by: Tzung-Bi Shih <tzungbi@chromium.org>

--- fs/nfs/nfs4proc.c
+++ fs/nfs/nfs4proc.c
@@ -7707,7 +7707,8 @@ static int nfs4_xattr_set_nfs4_acl(const struct xattr_handler *handler,
 
 static int nfs4_xattr_get_nfs4_acl(const struct xattr_handler *handler,
 				   struct dentry *unused, struct inode *inode,
-				   const char *key, void *buf, size_t buflen)
+				   const char *key, void *buf, size_t buflen,
+				   int flags)
 {
 	return nfs4_proc_get_acl(inode, buf, buflen, NFS4ACL_ACL);
 }
@@ -7731,7 +7732,8 @@ static int nfs4_xattr_set_nfs4_dacl(const struct xattr_handler *handler,
 
 static int nfs4_xattr_get_nfs4_dacl(const struct xattr_handler *handler,
 				    struct dentry *unused, struct inode *inode,
-				    const char *key, void *buf, size_t buflen)
+				    const char *key, void *buf, size_t buflen,
+				    int flags)
 {
 	return nfs4_proc_get_acl(inode, buf, buflen, NFS4ACL_DACL);
 }
@@ -7754,7 +7756,8 @@ static int nfs4_xattr_set_nfs4_sacl(const struct xattr_handler *handler,
 
 static int nfs4_xattr_get_nfs4_sacl(const struct xattr_handler *handler,
 				    struct dentry *unused, struct inode *inode,
-				    const char *key, void *buf, size_t buflen)
+				    const char *key, void *buf, size_t buflen,
+				    int flags)
 {
 	return nfs4_proc_get_acl(inode, buf, buflen, NFS4ACL_SACL);
 }
@@ -7782,7 +7785,8 @@ static int nfs4_xattr_set_nfs4_label(const struct xattr_handler *handler,
 
 static int nfs4_xattr_get_nfs4_label(const struct xattr_handler *handler,
 				     struct dentry *unused, struct inode *inode,
-				     const char *key, void *buf, size_t buflen)
+				     const char *key, void *buf, size_t buflen,
+				     int flags)
 {
 	if (security_ismaclabel(key))
 		return nfs4_get_security_label(inode, buf, buflen);
@@ -7860,7 +7864,8 @@ static int nfs4_xattr_set_nfs4_user(const struct xattr_handler *handler,
 
 static int nfs4_xattr_get_nfs4_user(const struct xattr_handler *handler,
 				    struct dentry *unused, struct inode *inode,
-				    const char *key, void *buf, size_t buflen)
+				    const char *key, void *buf, size_t buflen,
+				    int flags)
 {
 	u32 mask;
 	ssize_t ret;
