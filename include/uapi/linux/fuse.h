ae1cd7ccf8da1dc208f092c247723f0021ad3ee2
Alessio Balsini <balsini@google.com>
2021-01-25 17:02:28 +0000

    FROMLIST: fuse: Definitions and ioctl for passthrough
    Expose the FUSE_PASSTHROUGH interface to user space and declare all the
    basic data structures and functions as the skeleton on top of which the
    FUSE passthrough functionality will be built.
    
    As part of this, introduce the new FUSE passthrough ioctl, which allows
    the FUSE daemon to specify a direct connection between a FUSE file and a
    lower file system file. Such ioctl requires user space to pass the file
    descriptor of one of its opened files through the fuse_passthrough_out
    data structure introduced in this patch. This structure includes extra
    fields for possible future extensions.
    Also, add the passthrough functions for the set-up and tear-down of the
    data structures and locks that will be used both when fuse_conns and
    fuse_files are created/deleted.
    
    [CPNOTE: 20/05/21] Lee: Still fresh - hopefully this will land upstream soon
    
    Bug: 168023149
    Link: https://lore.kernel.org/lkml/20210125153057.3623715-4-balsini@android.com/
    Signed-off-by: Alessio Balsini <balsini@android.com>
    Change-Id: I732532581348adadda5b5048a9346c2b0868d539
    Signed-off-by: Alessio Balsini <balsini@google.com>
    
    [rebase515(groeck):
      Squashed:
        CHROMIUM: fuse: Drop unused variable
    ]
    Signed-off-by: Guenter Roeck <groeck@chromium.org>
    
    Change-Id: Id0ba00c88c8acc92c640369fc87d41c11f32b8d2

--- include/uapi/linux/fuse.h
+++ include/uapi/linux/fuse.h
@@ -392,6 +392,7 @@ struct fuse_file_lock {
 /* bits 32..63 get shifted down 32 bits into the flags2 field */
 #define FUSE_SECURITY_CTX	(1ULL << 32)
 #define FUSE_HAS_INODE_DAX	(1ULL << 33)
+#define FUSE_PASSTHROUGH	(1ULL << 34)
 
 /**
  * CUSE INIT request/reply flags
@@ -667,7 +668,7 @@ struct fuse_create_in {
 struct fuse_open_out {
 	uint64_t	fh;
 	uint32_t	open_flags;
-	uint32_t	padding;
+	uint32_t	passthrough_fh;
 };
 
 struct fuse_release_in {
@@ -877,6 +878,13 @@ struct fuse_in_header {
 	uint32_t	padding;
 };
 
+struct fuse_passthrough_out {
+	uint32_t	fd;
+	/* For future implementation */
+	uint32_t	len;
+	void		*vec;
+};
+
 struct fuse_out_header {
 	uint32_t	len;
 	int32_t		error;
@@ -957,6 +965,7 @@ struct fuse_notify_retrieve_in {
 /* Device ioctls: */
 #define FUSE_DEV_IOC_MAGIC		229
 #define FUSE_DEV_IOC_CLONE		_IOR(FUSE_DEV_IOC_MAGIC, 0, uint32_t)
+#define FUSE_DEV_IOC_PASSTHROUGH_OPEN	_IOW(FUSE_DEV_IOC_MAGIC, 1, struct fuse_passthrough_out)
 
 struct fuse_lseek_in {
 	uint64_t	fh;
