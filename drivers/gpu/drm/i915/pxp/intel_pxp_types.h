5c4ea8d9c16eb3a9005ea9067f58354a2382718e
Juston Li <juston.li@intel.com>
2021-09-30 10:22:03 -0700

    CHROMIUM: drm/i915/pxp: handle interrupted PXP TEE messages
    If a signal occurs during PXP TEE messages, MEI PXP exits and doesn't
    wait for the response. The send still went through though and a response
    will come causing the next PXP TEE message to send its own message but
    then receive the response from the previously interrupted message and we
    end up with all subsequent messages getting out of sync.
    
    Flag whenever a message is interrupted so on the next message we can
    drop that message before starting a new send/recv.
    
    Signed-off-by: Juston Li <juston.li@intel.com>
    Reviewed-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Signed-off-by: Alan Previn <alan.previn.teres.alexis@intel.com>
    Acked-by: Daniele Ceraolo Spurio <daniele.ceraolospurio@intel.com>
    
    BUG=b:257876317
    TEST=emerge-rex sys-kernel/chromeos-kernel-6_1
    
    Change-Id: I8f51e72864616db9307f0f7ec430be6b137a7053
    Signed-off-by: Sachin Kumar <sachin.kumar@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4607174
    Reviewed-by: Alan Previn Teres Alexis <alan.previn.teres.alexis@intel.corp-partner.google.com>
    Reviewed-by: Juston Li <justonli@google.com>
    Reviewed-by: Paz Zcharya <pazz@google.com>
    Tested-by: Hasan Abuzaina <hasan.m.abuzaina@intel.corp-partner.google.com>

--- drivers/gpu/drm/i915/pxp/intel_pxp_types.h
+++ drivers/gpu/drm/i915/pxp/intel_pxp_types.h
@@ -82,6 +82,13 @@ struct intel_pxp {
 	/* @dev_link: Enforce module relationship for power management ordering. */
 	struct device_link *dev_link;
 	/**
+	 * @mei_pxp_last_msg_interrupted: To catch and drop stale responses
+	 * from previuosly interrupted send-msg to mei before issuing new
+	 * send-recv.
+	 */
+	bool mei_pxp_last_msg_interrupted;
+
+	/**
 	 * @pxp_component_added: track if the pxp component has been added.
 	 * Set and cleared in tee init and fini functions respectively.
 	 */
