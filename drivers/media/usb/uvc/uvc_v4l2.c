c6b56f1c6fb01d19519e806bbe2c23f095327de1
Sergey Senozhatsky <senozhatsky@chromium.org>
2022-06-08 19:31:39 +0900

    CHROMIUM: media: uvcvideo: rollback ROI cache when SET_CUR fails
    We need to rollback ROI rectangle modifications when
    uvc_query_ctrl(SET_CUR) fails. This should not affect
    next ROI rectangle set operation, as cached rectangle
    will be overwritten with new data, but this will affect
    auto-controls set.
    
    BUG=b:233194103
    TEST=wrote a simple app that would attempt to SET_CUR invalid
         ROI rectangle (dimensions out of bounds) and verified that
         v4l2-ctl -c region_of_interest_auto_control=1 afterwards
         does not return ERANGE.
    TEST=teat app https://paste.googleplex.com/5199572868530176
    
    Reported-by: Ricardo Ribalda <ribalda@google.com>
    Signed-off-by: Sergey Senozhatsky <senozhatsky@chromium.org>
    Change-Id: I3c89b94add7a791aa72d20d85f832f6fa8e38628
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3691348
    Reviewed-by: Ricardo Ribalda <ribalda@chromium.org>
    (cherry picked from commit 230225d20e3bd92fa7b931e49c18e193eb72636a)
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/3703091
    Commit-Queue: Ricardo Ribalda <ribalda@chromium.org>

--- drivers/media/usb/uvc/uvc_v4l2.c
+++ drivers/media/usb/uvc/uvc_v4l2.c
@@ -1325,6 +1325,7 @@ static int uvc_ioctl_s_roi(struct file *file, void *fh,
 	struct uvc_fh *handle = fh;
 	struct uvc_streaming *stream = handle->stream;
 	struct uvc_video_chain *chain = handle->chain;
+	struct uvc_roi roi_backup;
 	struct uvc_roi *roi;
 	int ret;
 
@@ -1359,6 +1360,8 @@ static int uvc_ioctl_s_roi(struct file *file, void *fh,
 
 	validate_roi_bounds(stream, sel);
 
+	roi_backup = *roi;
+
 	/*
 	 * ROI left, top, right, bottom are global coordinates.
 	 * Note that we use ->auto_controls value which we read earlier.
@@ -1371,8 +1374,10 @@ static int uvc_ioctl_s_roi(struct file *file, void *fh,
 	ret = uvc_query_ctrl(stream->dev, UVC_SET_CUR, 1, stream->dev->intfnum,
 			     UVC_CT_REGION_OF_INTEREST_CONTROL, roi,
 			     sizeof(struct uvc_roi));
-	if (ret)
+	if (ret) {
+		*roi = roi_backup;
 		goto out;
+	}
 
 out:
 	mutex_unlock(&stream->mutex);
