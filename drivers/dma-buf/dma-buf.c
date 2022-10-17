1f053ebb85b51e2973087d7a4bad78d02b210035
Dmitry Osipenko <dmitry.osipenko@collabora.com>
2022-10-17 20:22:23 +0300

    UPSTREAM: dma-buf: Move dma_buf_vmap() to dynamic locking specification
    Move dma_buf_vmap/vunmap() functions to the dynamic locking
    specification by asserting that the reservation lock is held.
    
    Acked-by: Sumit Semwal <sumit.semwal@linaro.org>
    Acked-by: Christian König <christian.koenig@amd.com>
    Signed-off-by: Dmitry Osipenko <dmitry.osipenko@collabora.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20221017172229.42269-16-dmitry.osipenko@collabora.com
    (cherry picked from commit 34c7797f9ef498e722dfaebf2a35b75d70d8cf64)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:268680937
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: Ic42d75272c2ab1ef8216b7a8cb92e1a2bb6b7270

--- drivers/dma-buf/dma-buf.c
+++ drivers/dma-buf/dma-buf.c
@@ -1455,6 +1455,8 @@ int dma_buf_vmap(struct dma_buf *dmabuf, struct iosys_map *map)
 	if (WARN_ON(!dmabuf))
 		return -EINVAL;
 
+	dma_resv_assert_held(dmabuf->resv);
+
 	if (!dmabuf->ops->vmap)
 		return -EINVAL;
 
@@ -1519,6 +1521,8 @@ void dma_buf_vunmap(struct dma_buf *dmabuf, struct iosys_map *map)
 {
 	if (WARN_ON(!dmabuf))
 		return;
+
+	dma_resv_assert_held(dmabuf->resv);
 
 	BUG_ON(iosys_map_is_null(&dmabuf->vmap_ptr));
 	BUG_ON(dmabuf->vmapping_counter == 0);
