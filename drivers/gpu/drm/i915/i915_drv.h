04f36fb44c91976289a67676ef1182e998ac69c6
Jani Nikula <jani.nikula@intel.com>
2023-01-18 15:15:37 +0200

    UPSTREAM: drm/i915: move I915_COLOR_UNEVICTABLE to i915_gem_gtt.h
    Declutter i915_drv.h.
    
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Acked-by: Tvrtko Ursulin <tvrtko.ursulin@intel.com>
    Acked-by: Nirmoy Das <nirmoy.das@intel.com>
    Link: https://patchwork.freedesktop.org/patch/msgid/20230118131538.3558599-6-jani.nikula@intel.com
    (cherry picked from commit 0cfee2d7fd992d51cd68cc333dfebadc923abd92)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:280653303
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: Ibea2e9f29a92006fc0c75fdacf8ce7a9e4e44831

--- drivers/gpu/drm/i915/i915_drv.h
+++ drivers/gpu/drm/i915/i915_drv.h
@@ -68,8 +68,6 @@ struct drm_i915_clock_gating_funcs;
 struct vlv_s0ix_state;
 struct intel_pxp;
 
-#define I915_COLOR_UNEVICTABLE (-1) /* a non-vma sharing the address space */
-
 #define GEM_QUIRK_PIN_SWIZZLED_PAGES	BIT(0)
 
 /* Data Stolen Memory (DSM) aka "i915 stolen memory" */
