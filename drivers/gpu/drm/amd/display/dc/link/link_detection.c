9eb119fde8a4523ff954990d77ec8a5186962abc
Wenjing Liu <wenjing.liu@amd.com>
2023-02-14 12:16:55 -0500

    UPSTREAM: drm/amd/display: move dc_link functions in link root folder to dc_link_exports
    [why]
    link component should only have one interface serving dc.
    
    [how]
    We are moving dc_link functions exposed to DM to dc_link_exports
    and unify link component interface in link.h with function pointer
    to match the style of other dc component. This is the second step to move
    dc_link functions under link root folder to dc_link_exports.
    
    Reviewed-by: Jun Lei <Jun.Lei@amd.com>
    Acked-by: Qingqing Zhuo <qingqing.zhuo@amd.com>
    Signed-off-by: Wenjing Liu <wenjing.liu@amd.com>
    Tested-by: Daniel Wheeler <daniel.wheeler@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 76f5dc40ebb188b081e03783541856c03e97f8e0)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:300094055
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: I202c5766232157e42974a97a2156cd20eb781ae0

--- drivers/gpu/drm/amd/display/dc/link/link_detection.c
+++ drivers/gpu/drm/amd/display/dc/link/link_detection.c
@@ -1340,7 +1340,7 @@ static bool link_add_remote_sink_helper(struct dc_link *dc_link, struct dc_sink 
 	return true;
 }
 
-struct dc_sink *dc_link_add_remote_sink(
+struct dc_sink *link_add_remote_sink(
 		struct dc_link *link,
 		const uint8_t *edid,
 		int len,
@@ -1398,7 +1398,7 @@ fail_add_sink:
 	return NULL;
 }
 
-void dc_link_remove_remote_sink(struct dc_link *link, struct dc_sink *sink)
+void link_remove_remote_sink(struct dc_link *link, struct dc_sink *sink)
 {
 	int i;
 
