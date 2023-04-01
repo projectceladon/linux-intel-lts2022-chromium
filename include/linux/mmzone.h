4358529660afffbab2b93e8b33c44438be081c91
Yu Zhao <yuzhao@google.com>
2023-04-01 14:14:19 -0600

    BACKPORT: FROMLIST: mm: multi-gen LRU: use mmu_notifier_test_clear_young()
    An existing selftest can quickly demonstrate the effectiveness of this
    patch. On a generic workstation equipped with 64 CPUs and 256GB DRAM:
    
      $ sudo max_guest_memory_test -c 64 -m 256 -s 256
    
      MGLRU      run2
      ---------------
      Before    ~600s
      After      ~50s
      Off       ~250s
    
      kswapd (MGLRU before)
        100.00%  balance_pgdat
          100.00%  shrink_node
            100.00%  shrink_one
              99.97%  try_to_shrink_lruvec
                99.06%  evict_folios
                  97.41%  shrink_folio_list
                    31.33%  folio_referenced
                      31.06%  rmap_walk_file
                        30.89%  folio_referenced_one
                          20.83%  __mmu_notifier_clear_flush_young
                            20.54%  kvm_mmu_notifier_clear_flush_young
      =>                      19.34%  _raw_write_lock
    
      kswapd (MGLRU after)
        100.00%  balance_pgdat
          100.00%  shrink_node
            100.00%  shrink_one
              99.97%  try_to_shrink_lruvec
                99.51%  evict_folios
                  71.70%  shrink_folio_list
                    7.08%  folio_referenced
                      6.78%  rmap_walk_file
                        6.72%  folio_referenced_one
                          5.60%  lru_gen_look_around
      =>                    1.53%  __mmu_notifier_test_clear_young
    
      kswapd (MGLRU off)
        100.00%  balance_pgdat
          100.00%  shrink_node
            99.92%  shrink_lruvec
              69.95%  shrink_folio_list
                19.35%  folio_referenced
                  18.37%  rmap_walk_file
                    17.88%  folio_referenced_one
                      13.20%  __mmu_notifier_clear_flush_young
                        11.64%  kvm_mmu_notifier_clear_flush_young
      =>                  9.93%  _raw_write_lock
              26.23%  shrink_active_list
                25.50%  folio_referenced
                  25.35%  rmap_walk_file
                    25.28%  folio_referenced_one
                      23.87%  __mmu_notifier_clear_flush_young
                        23.69%  kvm_mmu_notifier_clear_flush_young
      =>                  18.98%  _raw_write_lock
    
    Signed-off-by: Yu Zhao <yuzhao@google.com>
    (am from https://patchwork.kernel.org/patch/13144315/)
    
    BUG=b:266976439
    UPSTREAM-TASK=b:266738578
    TEST=ran crostini.VimCompile
    
    Change-Id: I2d6c83ccad765d903d4c2d2e48ded7bd114942c8
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4392744
    Commit-Queue: Yu Zhao <yuzhao@chromium.org>
    Tested-by: Yu Zhao <yuzhao@chromium.org>
    Reviewed-by: T.J. Alumbaugh <talumbau@google.com>

--- include/linux/mmzone.h
+++ include/linux/mmzone.h
@@ -388,6 +388,7 @@ enum {
 	LRU_GEN_CORE,
 	LRU_GEN_MM_WALK,
 	LRU_GEN_NONLEAF_YOUNG,
+	LRU_GEN_SPTE_WALK,
 	NR_LRU_GEN_CAPS
 };
 
@@ -494,7 +495,7 @@ struct lru_gen_mm_walk {
 };
 
 void lru_gen_init_lruvec(struct lruvec *lruvec);
-void lru_gen_look_around(struct page_vma_mapped_walk *pvmw);
+bool lru_gen_look_around(struct page_vma_mapped_walk *pvmw);
 
 #ifdef CONFIG_MEMCG
 
@@ -582,8 +583,9 @@ static inline void lru_gen_init_lruvec(struct lruvec *lruvec)
 {
 }
 
-static inline void lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
+static inline bool lru_gen_look_around(struct page_vma_mapped_walk *pvmw)
 {
+	return false;
 }
 
 #ifdef CONFIG_MEMCG
