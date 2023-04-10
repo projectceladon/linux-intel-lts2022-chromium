a67697a9834a9d8dd1aab79505e7ab073e9f8a10
Jason Ling <jasonling@chromium.org>
2023-04-10 16:39:55 -0700

    CHROMIUM: add cros specific trace-points.
    !!!DO NOT UPSTREAM!!!
    !!!DO NOT PORT TO OTHER KERNELS!!!
    
    Add tracepoints to allow the monitoring of the following socket
    operations:
    
    sendmsg,recvmsg,listen,accept,release
    
    This is needed because fentry/fexit and LSM BPF hooks are not supported
    for ARM64 in kernels older than v6.6.
    
    It has been verified that the v6.6 kernel does not need this patch, both
    LSM BPF hooks and fentry/fexit hooks work fine. This is demonstrated by
    running the secagentd.NetworkEvents.relaxed_* tests on the strongbad
    platform.
    
    BUG=b:329882953
    TEST=Ran secagentd.NetworkEvents.relaxed_* on geralt
    UPSTREAM-TASK=b:278303728
    
    Change-Id: I2e6e56487566c87b773ddc6ed482fa039603726d
    Signed-off-by: Jason Ling <jasonling@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5441547
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>

--- net/ipv4/af_inet.c
+++ net/ipv4/af_inet.c
@@ -122,6 +122,12 @@
 
 #include <trace/events/sock.h>
 
+#define CREATE_TRACE_POINTS
+#include <trace/events/cros_net.h>
+EXPORT_TRACEPOINT_SYMBOL(cros_ip6_input_finish_enter);
+EXPORT_TRACEPOINT_SYMBOL(cros_ip6_finish_output2_enter);
+#undef CREATE_TRACE_POINTS
+
 /* The inetsw table contains everything that inet_create needs to
  * build a new socket.
  */
@@ -234,6 +240,9 @@ int inet_listen(struct socket *sock, int backlog)
 	err = 0;
 
 out:
+	// Cros specific tracepoint to accommodate the lack of
+	// arm64 bpf-trampoline in v5.10, v5.15 , v6.1 kernels.
+	trace_cros_inet_listen_exit(sock, backlog, err);
 	release_sock(sk);
 	return err;
 }
@@ -412,6 +421,7 @@ out_rcu_unlock:
 int inet_release(struct socket *sock)
 {
 	struct sock *sk = sock->sk;
+	trace_cros_inet_release_enter(sock);
 
 	if (sk) {
 		long timeout;
@@ -724,6 +734,7 @@ int __inet_stream_connect(struct socket *sock, struct sockaddr *uaddr,
 	sock->state = SS_CONNECTED;
 	err = 0;
 out:
+	trace_cros_inet_stream_connect_exit(sock, uaddr, addr_len, flags, is_sendmsg, err);
 	return err;
 
 sock_error:
@@ -778,6 +789,7 @@ int inet_accept(struct socket *sock, struct socket *newsock, int flags,
 	err = 0;
 	release_sock(sk2);
 do_err:
+	trace_cros_inet_accept_exit(sock, newsock, flags, kern, err);
 	return err;
 }
 EXPORT_SYMBOL(inet_accept);
@@ -837,6 +849,8 @@ int inet_sendmsg(struct socket *sock, struct msghdr *msg, size_t size)
 {
 	struct sock *sk = sock->sk;
 
+	trace_cros_inet_sendmsg_enter(sock, msg, size);
+
 	if (unlikely(inet_send_prepare(sk)))
 		return -EAGAIN;
 
@@ -863,6 +877,7 @@ EXPORT_SYMBOL_GPL(inet_splice_eof);
 ssize_t inet_sendpage(struct socket *sock, struct page *page, int offset,
 		      size_t size, int flags)
 {
+	ssize_t rv;
 	struct sock *sk = sock->sk;
 	const struct proto *prot;
 
@@ -871,9 +886,14 @@ ssize_t inet_sendpage(struct socket *sock, struct page *page, int offset,
 
 	/* IPV6_ADDRFORM can change sk->sk_prot under us. */
 	prot = READ_ONCE(sk->sk_prot);
-	if (prot->sendpage)
-		return prot->sendpage(sk, page, offset, size, flags);
-	return sock_no_sendpage(sock, page, offset, size, flags);
+	if (prot->sendpage) {
+		rv = prot->sendpage(sk, page, offset, size, flags);
+		trace_cros_inet_sendpage_exit(sock, page, offset, size, flags, rv);
+		return rv;
+	}
+	rv = sock_no_sendpage(sock, page, offset, size, flags);
+	trace_cros_inet_sendpage_exit(sock, page, offset, size, flags, rv);
+	return rv;
 }
 EXPORT_SYMBOL(inet_sendpage);
 
@@ -893,6 +913,7 @@ int inet_recvmsg(struct socket *sock, struct msghdr *msg, size_t size,
 			      sk, msg, size, flags, &addr_len);
 	if (err >= 0)
 		msg->msg_namelen = addr_len;
+	trace_cros_inet_recvmsg_exit(sock, msg, size, flags, err);
 	return err;
 }
 EXPORT_SYMBOL(inet_recvmsg);
