1a46e6ec597bd31668c7daf1201616195dc2f57e
Douglas Anderson <dianders@chromium.org>
2023-05-19 10:18:36 -0700

    UPSTREAM: watchdog/hardlockup: rename some "NMI watchdog" constants/function
    Do a search and replace of:
    - NMI_WATCHDOG_ENABLED => WATCHDOG_HARDLOCKUP_ENABLED
    - SOFT_WATCHDOG_ENABLED => WATCHDOG_SOFTOCKUP_ENABLED
    - watchdog_nmi_ => watchdog_hardlockup_
    - nmi_watchdog_available => watchdog_hardlockup_available
    - nmi_watchdog_user_enabled => watchdog_hardlockup_user_enabled
    - soft_watchdog_user_enabled => watchdog_softlockup_user_enabled
    - NMI_WATCHDOG_DEFAULT => WATCHDOG_HARDLOCKUP_DEFAULT
    
    Then update a few comments near where names were changed.
    
    This is specifically to make it less confusing when we want to introduce
    the buddy hardlockup detector, which isn't using NMIs.  As part of this,
    we sanitized a few names for consistency.
    
    [trix@redhat.com: make variables static]
      Link: https://lkml.kernel.org/r/20230525162822.1.I0fb41d138d158c9230573eaa37dc56afa2fb14ee@changeid
    Link: https://lkml.kernel.org/r/20230519101840.v5.12.I91f7277bab4bf8c0cb238732ed92e7ce7bbd71a6@changeid
    Signed-off-by: Douglas Anderson <dianders@chromium.org>
    Signed-off-by: Tom Rix <trix@redhat.com>
    Reviewed-by: Tom Rix <trix@redhat.com>
    Cc: Andi Kleen <ak@linux.intel.com>
    Cc: Catalin Marinas <catalin.marinas@arm.com>
    Cc: Chen-Yu Tsai <wens@csie.org>
    Cc: Christophe Leroy <christophe.leroy@csgroup.eu>
    Cc: Colin Cross <ccross@android.com>
    Cc: Daniel Thompson <daniel.thompson@linaro.org>
    Cc: "David S. Miller" <davem@davemloft.net>
    Cc: Guenter Roeck <groeck@chromium.org>
    Cc: Ian Rogers <irogers@google.com>
    Cc: Lecopzer Chen <lecopzer.chen@mediatek.com>
    Cc: Marc Zyngier <maz@kernel.org>
    Cc: Mark Rutland <mark.rutland@arm.com>
    Cc: Masayoshi Mizuma <msys.mizuma@gmail.com>
    Cc: Matthias Kaehlcke <mka@chromium.org>
    Cc: Michael Ellerman <mpe@ellerman.id.au>
    Cc: Nicholas Piggin <npiggin@gmail.com>
    Cc: Petr Mladek <pmladek@suse.com>
    Cc: Pingfan Liu <kernelfans@gmail.com>
    Cc: Randy Dunlap <rdunlap@infradead.org>
    Cc: "Ravi V. Shankar" <ravi.v.shankar@intel.com>
    Cc: Ricardo Neri <ricardo.neri@intel.com>
    Cc: Stephane Eranian <eranian@google.com>
    Cc: Stephen Boyd <swboyd@chromium.org>
    Cc: Sumit Garg <sumit.garg@linaro.org>
    Cc: Tzung-Bi Shih <tzungbi@chromium.org>
    Cc: Will Deacon <will@kernel.org>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    (cherry picked from commit df95d3085caa5b99a60eb033d7ad6c2ff2b43dbf)
    
    BUG=b:172213097
    TEST=echo HARDLOCKUP > /sys/kernel/debug/provoke-crash/DIRECT
    
    Change-Id: I91f7277bab4bf8c0cb238732ed92e7ce7bbd71a6
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4673824
    Reviewed-by: Sean Paul <sean@poorly.run>
    Commit-Queue: Douglas Anderson <dianders@chromium.org>
    Reviewed-by: Guenter Roeck <groeck@chromium.org>
    Tested-by: Douglas Anderson <dianders@chromium.org>

--- arch/powerpc/platforms/pseries/mobility.c
+++ arch/powerpc/platforms/pseries/mobility.c
@@ -755,7 +755,7 @@ static int pseries_migrate_partition(u64 handle)
 		goto out;
 
 	if (factor)
-		watchdog_nmi_set_timeout_pct(factor);
+		watchdog_hardlockup_set_timeout_pct(factor);
 
 	ret = pseries_suspend(handle);
 	if (ret == 0) {
@@ -771,7 +771,7 @@ static int pseries_migrate_partition(u64 handle)
 		pseries_cancel_migration(handle, ret);
 
 	if (factor)
-		watchdog_nmi_set_timeout_pct(0);
+		watchdog_hardlockup_set_timeout_pct(0);
 
 out:
 	vas_migration_handler(VAS_RESUME);
