feef345e727f7d70787693a059a3215bf89abec3
Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
2023-06-06 05:22:21 +0000

    Revert "BACKPORT: FROMGIT: drm/i915/pxp: Enable PXP with MTL-GSC-CS"
    This reverts commit 193b786424647e5abc30e08e9acf320f1556db00
    for clean backport . Will restore same in top of series
    
    BUG=b:265114629
    TEST=i915 driver loaded successfully
    
    Change-Id: I102b381fad5405110a78644cc0cdde65a4ec4b44
    Signed-off-by: Ap, Kamal <kamal.ap@intel.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4592774
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Tested-by: Waheem Md <waheem.md@intel.com>

--- drivers/gpu/drm/i915/i915_pci.c
+++ drivers/gpu/drm/i915/i915_pci.c
@@ -1148,7 +1148,6 @@ static const struct intel_device_info mtl_info = {
 	.has_guc_deprivilege = 1,
 	.has_mslice_steering = 0,
 	.has_snoop = 1,
-	.has_pxp = 1,
 	.__runtime.memory_regions = REGION_SMEM | REGION_STOLEN_LMEM,
 	.__runtime.platform_engine_mask = BIT(RCS0) | BIT(BCS0) | BIT(CCS0),
 	.require_force_probe = 1,
