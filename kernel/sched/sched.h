05a4ee5afcac435d597817f228caf503cebb3ae9
Tim C Chen <tim.c.chen@linux.intel.com>
2023-07-07 15:57:01 -0700

    UPSTREAM: sched/topology: Record number of cores in sched group
    When balancing sibling domains that have different number of cores,
    tasks in respective sibling domain should be proportional to the
    number of cores in each domain. In preparation of implementing such a
    policy, record the number of cores in a scheduling group.
    
    Signed-off-by: Tim Chen <tim.c.chen@linux.intel.com>
    Signed-off-by: Peter Zijlstra (Intel) <peterz@infradead.org>
    Link: https://lore.kernel.org/r/04641eeb0e95c21224352f5743ecb93dfac44654.1688770494.git.tim.c.chen@linux.intel.com
    (cherry picked from commit d24cb0d9113f5932b8832533ce82351b5911ed50)
    
    BUG=b:242115011
    TEST=Build and Boot Sanity
    
    Change-Id: I2d8be5860351bdb365e764fef05192514773c3b3
    Signed-off-by: Sachin Kumar <sachin.kumar@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4798015
    Tested-by: Basavaprasad Kanshetty <basavaprasad.kanshetty@intel.corp-partner.google.com>
    Reviewed-by: Paz Zcharya <pazz@google.com>
    Reviewed-by: Sean Paul <sean@poorly.run>
    Tested-by: Adeel Arshad <adeel.arshad@intel.com>

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -1854,6 +1854,7 @@ struct sched_group {
 	atomic_t		ref;
 
 	unsigned int		group_weight;
+	unsigned int		cores;
 	struct sched_group_capacity *sgc;
 	int			asym_prefer_cpu;	/* CPU of highest priority in group */
 	int			flags;
