a75a3306734818049d5382a7ebd41dd1871cf6a1
Juston Li <justonli@google.com>
2023-07-25 20:06:47 +0000

    CHROMIUM: drm/i915/pxp: sanitize session_id index
    session_id is derived from user passed pxp_tag, sanitize it with
    array_index_nospec() to protect against speculative OOB array indexes.
    
    smatch reported the following warnings:
    drivers/gpu/drm/i915/pxp/intel_pxp_session.c:280 intel_pxp_sm_ioctl_query_pxp_tag() warn: potential spectre issue 'pxp->hwdrm_sessions' [r] (local cap)
    drivers/gpu/drm/i915/pxp/intel_pxp_session.c:311 intel_pxp_sm_ioctl_mark_session_in_play() warn: potential spectre issue 'pxp->hwdrm_sessions' [w] (local cap)
    
    Fixes: c4c8a083bf40 ("CHROMIUM: drm/i915/pxp: Validate pxp_tag inputs in PXP_OPS ioctls")
    Fixes: caaec6ca2f09 ("CHROMIUM: drm/i915/pxp: Validate pxp_tag inputs in PXP_OPS ioctls")
    Signed-off-by: Juston Li <justonli@google.com>
    
    BUG=b:290628212, b:288032697
    TEST=sudo emerge-nissa chromeos-kernel-5_15
         run smatch kchecker
    UPSTREAM-TASK=b:185364793
    
    Change-Id: Ia790caf59c50a289b0fff7b0df60cfd5350e4d6c
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4717942
    Reviewed-by: Drew Davenport <ddavenport@chromium.org>
    Reviewed-by: Alan Previn Teres Alexis <alan.previn.teres.alexis@intel.corp-partner.google.com>
    Tested-by: Juston Li <justonli@google.com>
    Commit-Queue: Juston Li <justonli@google.com>
    Signed-off-by: Sachin Kumar <sachin.kumar@intel.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4825549
    Reviewed-by: Juston Li <justonli@google.com>
    Tested-by: Hasan Abuzaina <hasan.m.abuzaina@intel.corp-partner.google.com>
    Reviewed-by: Paz Zcharya <pazz@google.com>

--- drivers/gpu/drm/i915/pxp/intel_pxp_session.c
+++ drivers/gpu/drm/i915/pxp/intel_pxp_session.c
@@ -3,6 +3,9 @@
  * Copyright(c) 2020, Intel Corporation. All rights reserved.
  */
 
+#include <drm/i915_drm.h>
+#include <linux/nospec.h>
+
 #include "i915_drv.h"
 
 #include "intel_pxp.h"
@@ -239,6 +242,7 @@ int intel_pxp_sm_ioctl_terminate_session(struct intel_pxp *pxp,
 	if (session_id >= INTEL_PXP_MAX_HWDRM_SESSIONS)
 		return -EINVAL;
 
+	session_id = array_index_nospec(session_id, INTEL_PXP_MAX_HWDRM_SESSIONS);
 	if (!pxp->hwdrm_sessions[session_id])
 		return 0;
 
@@ -266,6 +270,7 @@ int intel_pxp_sm_ioctl_query_pxp_tag(struct intel_pxp *pxp,
 	if (session_id >= INTEL_PXP_MAX_HWDRM_SESSIONS)
 		return -EINVAL;
 
+	session_id = array_index_nospec(session_id, INTEL_PXP_MAX_HWDRM_SESSIONS);
 	if (!pxp->hwdrm_sessions[session_id]) {
 		*pxp_tag = 0;
 		*session_is_alive = 0;
@@ -297,6 +302,7 @@ int intel_pxp_sm_ioctl_mark_session_in_play(struct intel_pxp *pxp,
 	if (session_id >= INTEL_PXP_MAX_HWDRM_SESSIONS)
 		return -EINVAL;
 
+	session_id = array_index_nospec(session_id, INTEL_PXP_MAX_HWDRM_SESSIONS);
 	if (!pxp->hwdrm_sessions[session_id])
 		return -EINVAL;
 
