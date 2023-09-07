7e3499c390f364354013d325f5363d009afab468
Drew Davenport <ddavenport@chromium.org>
2023-09-07 14:27:25 -0600

    UPSTREAM: drm/amd: Load SMU microcode during early_init
    This will ensure that the microcode is available before the firmware
    framebuffer has been destroyed.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Reviewed-by: Lijo Lazar <lijo.lazar@amd.com>
    Signed-off-by: Mario Limonciello <mario.limonciello@amd.com>
    Signed-off-by: Alex Deucher <alexander.deucher@amd.com>
    (cherry picked from commit 32806038aa7634635eb46f941979b6f6fda5a462)
    Signed-off-by: Drew Davenport <ddavenport@chromium.org>
    
    BUG=b:280653303
    TEST=build test
    
    Backported using forklift.py
    
    Change-Id: I5b87c79fa616663b261ae3ccc807da263099330f

--- drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
+++ drivers/gpu/drm/amd/pm/swsmu/amdgpu_smu.c
@@ -642,6 +642,7 @@ static int smu_early_init(void *handle)
 {
 	struct amdgpu_device *adev = (struct amdgpu_device *)handle;
 	struct smu_context *smu;
+	int r;
 
 	smu = kzalloc(sizeof(struct smu_context), GFP_KERNEL);
 	if (!smu)
@@ -659,7 +660,10 @@ static int smu_early_init(void *handle)
 	adev->powerplay.pp_handle = smu;
 	adev->powerplay.pp_funcs = &swsmu_pm_funcs;
 
-	return smu_set_funcs(adev);
+	r = smu_set_funcs(adev);
+	if (r)
+		return r;
+	return smu_init_microcode(smu);
 }
 
 static int smu_set_default_dpm_table(struct smu_context *smu)
@@ -1131,12 +1135,6 @@ static int smu_sw_init(void *handle)
 
 	smu->smu_dpm.dpm_level = AMD_DPM_FORCED_LEVEL_AUTO;
 	smu->smu_dpm.requested_dpm_level = AMD_DPM_FORCED_LEVEL_AUTO;
-
-	ret = smu_init_microcode(smu);
-	if (ret) {
-		dev_err(adev->dev, "Failed to load smu firmware!\n");
-		return ret;
-	}
 
 	INIT_DELAYED_WORK(&smu->swctf_delayed_work,
 			  smu_swctf_delayed_work_handler);
