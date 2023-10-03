hentaiOS-Devices/kernel_google_gs

CHROMIUM: net: wwan: t7xx: Add support for cold reboot
This allows us to recover from a state where the device doesn't respond
to any commands. It calls the FHRF and SHRF ACPI methods, essentially
performing a cold reboot.
The reset can be triggered using a dedicated sysfs file - "cold_reboot".
For example:
"echo 1 > /sys/devices/pci0000:00/0000:00:1c.0/0000:57:00.0/cold_reboot"

BUG=b:241850118
TEST=Trigger the reboot and verify that the modem is working
UPSTREAM-TASK=b:269405297

Signed-off-by: Kornel Dulęba <korneld@google.com>
Change-Id: I25b3afc9d4e96a1c65bd44cc8611a48b942e722b
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4218573
Reviewed-by: Radoslaw Biernacki <biernacki@google.com>
Commit-Queue: Radoslaw Biernacki <biernacki@google.com>

drivers/net/wwan/t7xx/t7xx_pci_rescan.c
@@ -7,6 +7,7 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ":t7xx:%s: " fmt, __func__
 #define dev_fmt(fmt) "t7xx: " fmt
 
+#include <linux/acpi.h>
 #include <linux/delay.h>
 #include <linux/pci.h>
 #include <linux/spinlock.h>
@@ -47,15 +48,41 @@ static void t7xx_remove_rescan(struct work_struct *work)
 	struct pci_dev *pdev;
 	int num_retries = RESCAN_RETRIES;
 	unsigned long flags;
+#ifdef CONFIG_ACPI
+	acpi_status status;
+	acpi_handle handle;
+	bool cold_reboot;
+#endif
 
 	spin_lock_irqsave(&g_mtk_rescan_context.dev_lock, flags);
 	g_mtk_rescan_context.rescan_done = 0;
+#ifdef CONFIG_ACPI
+	cold_reboot = g_mtk_rescan_context.cold_reboot;
+#endif
 	pdev = g_mtk_rescan_context.dev;
 	spin_unlock_irqrestore(&g_mtk_rescan_context.dev_lock, flags);
 
 	if (pdev) {
-		pci_stop_and_remove_bus_device_locked(pdev);
+#ifdef CONFIG_ACPI
+		handle = ACPI_HANDLE(&pdev->dev);
+#endif
 		pr_debug("start remove and rescan flow\n");
+		pci_stop_and_remove_bus_device_locked(pdev);
+#ifdef CONFIG_ACPI
+		if (cold_reboot) {
+			pr_info("Performing cold modem reboot\n");
+			status = acpi_execute_simple_method(handle, "FHRF", 1);
+			if (ACPI_FAILURE(status)) {
+				dev_err(&pdev->dev, "Failed to call _FHRF: %d\n",
+					status);
+			}
+			status = acpi_evaluate_object(handle, "SHRF", NULL, NULL);
+			if (ACPI_FAILURE(status)) {
+				dev_err(&pdev->dev, "Failed to call _SHRF: %d\n",
+					status);
+			}
+		}
+#endif
 	}
 
 	do {
@@ -71,7 +98,7 @@ static void t7xx_remove_rescan(struct work_struct *work)
 	} while (num_retries--);
 }
 
-void t7xx_rescan_queue_work(struct pci_dev *pdev)
+void t7xx_rescan_queue_work(struct pci_dev *pdev, bool cold_reboot)
 {
 	unsigned long flags;
 
@@ -84,6 +111,7 @@ void t7xx_rescan_queue_work(struct pci_dev *pdev)
 	}
 
 	g_mtk_rescan_context.dev = pdev;
+	g_mtk_rescan_context.cold_reboot = cold_reboot;
 	spin_unlock_irqrestore(&g_mtk_rescan_context.dev_lock, flags);
 	queue_work(g_mtk_rescan_context.pcie_rescan_wq, &g_mtk_rescan_context.service_task);
 }

drivers/net/wwan/t7xx/t7xx_port_devlink.c
@@ -463,7 +463,7 @@ static int t7xx_devlink_reload_up(struct devlink *devlink,
 	switch (action) {
 	case DEVLINK_RELOAD_ACTION_DRIVER_REINIT:
 	case DEVLINK_RELOAD_ACTION_FW_ACTIVATE:
-		t7xx_rescan_queue_work(dl->mtk_dev->pdev);
+		t7xx_rescan_queue_work(dl->mtk_dev->pdev, false);
 		return 0;
 	default:
 		/* Unsupported action should not get to this function */

projectceladon/linux-intel-lts2022-chromium

BUG=b:241850118, b:302552686
(cherry picked from commit c9f39b9e95117e74b4fac5c704ffd8032779abcc)
Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4903302
Reviewed-by: Rukun Mao <rmao@google.com>
Reviewed-by: Daniel Winkler <danielwinkler@google.com>
Tested-by: Rukun Mao <rmao@google.com>

drivers/net/wwan/t7xx/t7xx_pci_rescan.c
@@ -7,6 +7,7 @@
 #define pr_fmt(fmt) KBUILD_MODNAME ":t7xx:%s: " fmt, __func__
 #define dev_fmt(fmt) "t7xx: " fmt
 
+#include <linux/acpi.h>
 #include <linux/delay.h>
 #include <linux/pci.h>
 #include <linux/spinlock.h>
@@ -41,13 +42,37 @@ static void t7xx_remove_rescan(struct work_struct *work)
 {
 	int num_retries = RESCAN_RETRIES;
 	struct pci_dev *pdev;
+#ifdef CONFIG_ACPI
+	acpi_status status;
+	acpi_handle handle;
+	bool cold_reboot;
+#endif
 
 	atomic_set(&t7xx_rescan_ctx.rescan_done, 0);
 	pdev = t7xx_rescan_ctx.dev;
+#ifdef CONFIG_ACPI
+	cold_reboot = t7xx_rescan_ctx.cold_reboot;
+#endif
 
 	if (pdev) {
-		pci_stop_and_remove_bus_device_locked(pdev);
 		pr_debug("start remove and rescan flow\n");
+		pci_stop_and_remove_bus_device_locked(pdev);
+#ifdef CONFIG_ACPI
+		handle = ACPI_HANDLE(&pdev->dev);
+		if (cold_reboot) {
+			pr_info("Performing cold modem reboot\n");
+			status = acpi_execute_simple_method(handle, "FHRF", 1);
+			if (ACPI_FAILURE(status)) {
+				dev_err(&pdev->dev, "Failed to call _FHRF: %d\n",
+					status);
+			}
+			status = acpi_evaluate_object(handle, "SHRF", NULL, NULL);
+			if (ACPI_FAILURE(status)) {
+				dev_err(&pdev->dev, "Failed to call _SHRF: %d\n",
+					status);
+			}
+		}
+#endif
 	}
 
 	do {
@@ -60,7 +85,7 @@ static void t7xx_remove_rescan(struct work_struct *work)
 	} while (num_retries--);
 }
 
-void t7xx_rescan_queue_work(struct pci_dev *pdev)
+void t7xx_rescan_queue_work(struct pci_dev *pdev, bool cold_reboot)
 {
 	if (!atomic_read(&t7xx_rescan_ctx.rescan_done)) {
 		dev_err(&pdev->dev, "Rescan failed\n");
@@ -68,6 +93,7 @@ void t7xx_rescan_queue_work(struct pci_dev *pdev)
 	}
 
 	t7xx_rescan_ctx.dev = pdev;
+	t7xx_rescan_ctx.cold_reboot = cold_reboot;
 	queue_work(t7xx_rescan_ctx.pcie_rescan_wq, &t7xx_rescan_ctx.service_task);
 }
 

drivers/net/wwan/t7xx/t7xx_port_devlink.c
@@ -442,7 +442,7 @@ static int t7xx_devlink_reload_up(struct devlink *devlink,
 	switch (action) {
 	case DEVLINK_RELOAD_ACTION_DRIVER_REINIT:
 	case DEVLINK_RELOAD_ACTION_FW_ACTIVATE:
-		t7xx_rescan_queue_work(dl->t7xx_dev->pdev);
+		t7xx_rescan_queue_work(dl->t7xx_dev->pdev, false);
 		return 0;
 	default:
 		/* Unsupported action should not get to this function */