b3d3417d2226ed8cb54b3d584b8278577c984c34
Copy Chou <copy.chou@mediatek.corp-partner.google.com>
2024-01-10 10:01:38 +0800

    CHROMIUM: include: uapi: linux: add v4l2_fourcc
    [media] v4l2_fourcc: add V4L2_META_FMT_MTFD_RESULT
    
    The Meta data is used for MediaTek face detection
    
    BUG=b:236339617
    TEST=emerge-geralt sys-kernel/chromeos-kernel-6_1
    UPSTREAM-TASK=b:297128621
    
    Change-Id: Ic18ae6dd5b905db169d0d2751cdec5856c9d3612
    Signed-off-by: Copy Chou <copy.chou@mediatek.corp-partner.google.com>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5182995
    Tested-by: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>

--- include/uapi/linux/videodev2.h
+++ include/uapi/linux/videodev2.h
@@ -843,6 +843,9 @@ struct v4l2_pix_format {
 #define V4L2_META_FMT_RK_ISP1_PARAMS	v4l2_fourcc('R', 'K', '1', 'P') /* Rockchip ISP1 3A Parameters */
 #define V4L2_META_FMT_RK_ISP1_STAT_3A	v4l2_fourcc('R', 'K', '1', 'S') /* Rockchip ISP1 3A Statistics */
 
+/* Vendor-specific definition: used for the MediaTek camera subsystem's face detection results */
+#define V4L2_META_FMT_MTFD_RESULT v4l2_fourcc('M', 'T', 'f', 'd')
+
 /* priv field value to indicates that subsequent fields are valid. */
 #define V4L2_PIX_FMT_PRIV_MAGIC		0xfeedcafe
 
