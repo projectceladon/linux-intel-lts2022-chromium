aa257c819efcd4bf3d60fb2dd62f755c572b16e0
Lee Shawn C <shawn.c.lee@intel.com>
2024-01-17 11:04:26 +0800

    Revert "UPSTREAM: drm/i915/mtl: Update workaround 14018778641"
    This reverts commit 065eee0d9474c100ed9ac8fdb85e47a7d5ae8205.
    
    Reason for revert: this patch caused Android CTS CtsDeqpTestCases
    failed. It may need some times to figure out proper fix from upstream
    developer. So revert this CL to pass CTS and unblock FSI.
    Signed-off-by: Lee Shawn C <shawn.c.lee@intel.com>
    
    BUG=b:317957068
    TEST= Android CTS can pass all CtsDeqpTestCases.
    
    Change-Id: If638775892827a71c2c377566de4bd0eefd19499
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5203053
    Tested-by: Kazuhiro Inaba <kinaba@chromium.org>
    Reviewed-by: Kazuhiro Inaba <kinaba@chromium.org>

--- drivers/gpu/drm/i915/gt/intel_workarounds.c
+++ drivers/gpu/drm/i915/gt/intel_workarounds.c
@@ -1635,6 +1635,7 @@ static void
 xelpg_gt_workarounds_init(struct intel_gt *gt, struct i915_wa_list *wal)
 {
 	/* Wa_14018778641 / Wa_18018781329 */
+	wa_mcr_write_or(wal, RENDER_MOD_CTRL, FORCE_MISS_FTLB);
 	wa_mcr_write_or(wal, COMP_MOD_CTRL, FORCE_MISS_FTLB);
 
 	/* Wa_22016670082 */
@@ -1681,6 +1682,8 @@ xelpmp_gt_workarounds_init(struct intel_gt *gt, struct i915_wa_list *wal)
 	 * GT, the media GT's versions are regular singleton registers.
 	 */
 	wa_write_or(wal, XELPMP_GSC_MOD_CTRL, FORCE_MISS_FTLB);
+	wa_write_or(wal, XELPMP_VDBX_MOD_CTRL, FORCE_MISS_FTLB);
+	wa_write_or(wal, XELPMP_VEBX_MOD_CTRL, FORCE_MISS_FTLB);
 
 	/* Wa_22016670082 */
 	wa_write_or(wal, GEN12_SQCNT1, GEN12_STRICT_RAR_ENABLE);
