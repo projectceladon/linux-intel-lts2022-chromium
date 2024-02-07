066549793939812078f6ca50c9a9fde7ab13f406
Michael Lo <michael.lo@mediatek.com>
2024-02-07 09:39:45 +0800

    FROMLIST: wifi: mt76: mt7921: fix suspend issue on MediaTek COB platform
    MediaTek's controller driver on COB platform (e.g. MT8188) is
    capable of controlling power supplies and reset pin of a component
    (e.g. a WIFI chip) in power-on and power-off process.
    
    Due to this optional feature, mt76 need to inform controller
    that mt76 need to keep power during suspend. Otherwise WIFI will be
    powered off when system enters suspend process.
    
    The "wakeup-source" property was used for the device that need
    this to go into suspend mode so that mt76 suspend handler doesn't
    fail and the system is able to enter into a suspend state.
    
    An example:
    wifi: mt7921@0 {
    	wifi0{
    		reg = <0x0000 0 0 0 0>;
    		wakeup-source;
    	};
    };
    
    Signed-off-by: Michael Lo <michael.lo@mediatek.com>
    Signed-off-by: Ming Yen Hsieh <MingYen.Hsieh@mediatek.com>
    (am from https://patchwork.kernel.org/patch/13547981/)
    (also found at https://lore.kernel.org/r/20240207013945.23558-1-mingyen.hsieh@mediatek.com)
    
    BUG=b:313371758
    UPSTREAM-TASK=b:313371758
    TEST=powerd_dbus_suspend works with wifi
    
    Change-Id: I3f486f9b8a01c356148321261f4d9f53893e335b
    Signed-off-by: Fei Shao <fshao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5313160
    Reviewed-by: Chen-Yu Tsai <wenst@google.com>

--- drivers/net/wireless/mediatek/mt76/mt7921/pci.c
+++ drivers/net/wireless/mediatek/mt76/mt7921/pci.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/module.h>
 #include <linux/pci.h>
+#include <linux/of.h>
 
 #include "mt7921.h"
 #include "mac.h"
@@ -376,6 +377,9 @@ static int mt7921_pci_probe(struct pci_dev *pdev,
 	if (ret)
 		goto err_free_irq;
 
+	if (of_property_read_bool(dev->mt76.dev->of_node, "wakeup-source"))
+		device_init_wakeup(dev->mt76.dev, true);
+
 	return 0;
 
 err_free_irq:
@@ -392,6 +396,9 @@ static void mt7921_pci_remove(struct pci_dev *pdev)
 {
 	struct mt76_dev *mdev = pci_get_drvdata(pdev);
 	struct mt7921_dev *dev = container_of(mdev, struct mt7921_dev, mt76);
+
+	if (of_property_read_bool(dev->mt76.dev->of_node, "wakeup-source"))
+		device_init_wakeup(dev->mt76.dev, false);
 
 	mt7921e_unregister_device(dev);
 	devm_free_irq(&pdev->dev, pdev->irq, dev);
