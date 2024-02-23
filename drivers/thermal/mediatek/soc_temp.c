cf34e94771d0f5d68c6eea14334266025d432e25
Fei Shao <fshao@chromium.org>
2024-02-23 08:28:07 +0000

    Revert "FIXUP: FROMLIST: thermal: mediatek: add another get_temp ops for thermal sensors"
    This reverts commit fe9921bbba8e1936b7aa2b9c263a7e5a18d2ae18.
    
    Reason for revert: Preparation for latest MTK thermal LTVS support
    
    Original change's description:
    > FIXUP: FROMLIST: thermal: mediatek: add another get_temp ops for thermal sensors
    >
    > The original patch changed the thermal zone device's private data, i.e.
    > tz->devdata, adding one layer of indirection. It seems that during the
    > rebase, the change on the thermal zone registration side was preserved,
    > but on the temperature readout side it was not. This causes the
    > temperature readout to crash, as it tries to dereference a pointer of
    > an incorrect type.
    >
    > Fix this by adding back the change in mtk_read_temp() and
    > mtk_read_sensor_temp().
    >
    > BUG=b:236331727
    > TEST=Build and boot on kukui / jacuzzi
    > UPSTREAM-TASK=b:204226007, b:236331727
    >
    > Change-Id: I95dc0686f7e773a8efbd840c30352fe7895f7037
    > Signed-off-by: Chen-Yu Tsai <wenst@chromium.org>
    > Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/4384325
    > Reviewed-by: Fei Shao <fshao@chromium.org>
    
    BUG=b:236331727
    BUG=b:326555352
    
    Change-Id: I2c3ca3f1c124a4451ff07fa583a9061430473d20
    Signed-off-by: Fei Shao <fshao@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5320304
    Reviewed-by: Pin-yen Lin <treapking@chromium.org>

--- drivers/thermal/mediatek/soc_temp.c
+++ drivers/thermal/mediatek/soc_temp.c
@@ -901,8 +901,7 @@ static int mtk_thermal_bank_temperature(struct mtk_thermal_bank *bank)
 
 static int mtk_read_temp(struct thermal_zone_device *tz, int *temperature)
 {
-	struct mtk_thermal_zone *mtz = tz->devdata;
-	struct mtk_thermal *mt = mtz->mt;
+	struct mtk_thermal *mt = tz->devdata;
 	int i;
 	int tempmax = INT_MIN;
 
@@ -922,8 +921,7 @@ static int mtk_read_temp(struct thermal_zone_device *tz, int *temperature)
 
 static int mtk_read_sensor_temp(struct thermal_zone_device *tz, int *temperature)
 {
-	struct mtk_thermal_zone *mtz = tz->devdata;
-	struct mtk_thermal *mt = mtz->mt;
+	struct mtk_thermal *mt = tz->devdata;
 	const struct mtk_thermal_data *conf = mt->conf;
 	int id = tz->id - 1;
 	int temp = INT_MIN;
