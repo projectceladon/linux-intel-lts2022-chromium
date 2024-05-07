057f7edf863d6f39d971468df5ab49419646ddde
Hsin-Te Yuan <yuanhsinte@chromium.org>
2024-05-07 05:25:51 +0000

    Revert "UPSTREAM: thermal/drivers/mediatek/lvts_thermal: Fix error check in lvts_debugfs_init()"
    This reverts commit 89ad9cb90e782abc6baa425dbaaa0f41785a4eae.
    
    BUG=b:236331727
    BUG=b:326555352
    TEST=emerge-asurada-kernelnext chromeos-kernel-6_1
    
    Change-Id: I7997b0c361e63ae89d1c6a22cde84827970ac509
    Signed-off-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5520369
    Reviewed-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Tested-by: Fei Shao <fshao@chromium.org>

--- drivers/thermal/mediatek/lvts_thermal.c
+++ drivers/thermal/mediatek/lvts_thermal.c
@@ -219,7 +219,7 @@ static int lvts_debugfs_init(struct device *dev, struct lvts_domain *lvts_td)
 
 		sprintf(name, "controller%d", i);
 		dentry = debugfs_create_dir(name, lvts_td->dom_dentry);
-		if (IS_ERR(dentry))
+		if (!dentry)
 			continue;
 
 		regset = devm_kzalloc(dev, sizeof(*regset), GFP_KERNEL);
