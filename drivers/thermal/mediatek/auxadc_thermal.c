27ababe0645e81bbff075f8a9a7eec9442593d77
Hsin-Te Yuan <yuanhsinte@chromium.org>
2024-05-07 05:25:54 +0000

    Revert "UPSTREAM: thermal/drivers/mediatek: Control buffer enablement tweaks"
    This reverts commit 311c1da1f825144a967babc82e460af454a30b62.
    
    BUG=b:236331727
    BUG=b:326555352
    TEST=emerge-asurada-kernelnext chromeos-kernel-6_1
    
    Change-Id: Icfae2fc811da5d52ec5232fae565b512a63b46c2
    Signed-off-by: Hsin-Te Yuan <yuanhsinte@chromium.org>
    Reviewed-on: https://chromium-review.googlesource.com/c/chromiumos/third_party/kernel/+/5520390
    Tested-by: Fei Shao <fshao@chromium.org>
    Commit-Queue: Fei Shao <fshao@chromium.org>
    Reviewed-by: Fei Shao <fshao@chromium.org>

--- drivers/thermal/mediatek/auxadc_thermal.c
+++ drivers/thermal/mediatek/auxadc_thermal.c
@@ -307,9 +307,6 @@ struct mtk_thermal_data {
 	bool need_switch_bank;
 	struct thermal_bank_cfg bank_data[MAX_NUM_ZONES];
 	enum mtk_thermal_version version;
-	u32 apmixed_buffer_ctl_reg;
-	u32 apmixed_buffer_ctl_mask;
-	u32 apmixed_buffer_ctl_set;
 };
 
 struct mtk_thermal {
@@ -563,9 +560,6 @@ static const struct mtk_thermal_data mt7622_thermal_data = {
 	.adcpnp = mt7622_adcpnp,
 	.sensor_mux_values = mt7622_mux_values,
 	.version = MTK_THERMAL_V2,
-	.apmixed_buffer_ctl_reg = APMIXED_SYS_TS_CON1,
-	.apmixed_buffer_ctl_mask = GENMASK(31, 6) | BIT(3),
-	.apmixed_buffer_ctl_set = BIT(0),
 };
 
 /*
@@ -1085,18 +1079,14 @@ static const struct of_device_id mtk_thermal_of_match[] = {
 };
 MODULE_DEVICE_TABLE(of, mtk_thermal_of_match);
 
-static void mtk_thermal_turn_on_buffer(struct mtk_thermal *mt,
-				       void __iomem *apmixed_base)
+static void mtk_thermal_turn_on_buffer(void __iomem *apmixed_base)
 {
-	u32 tmp;
+	int tmp;
 
-	if (!mt->conf->apmixed_buffer_ctl_reg)
-		return;
-
-	tmp = readl(apmixed_base + mt->conf->apmixed_buffer_ctl_reg);
-	tmp &= mt->conf->apmixed_buffer_ctl_mask;
-	tmp |= mt->conf->apmixed_buffer_ctl_set;
-	writel(tmp, apmixed_base + mt->conf->apmixed_buffer_ctl_reg);
+	tmp = readl(apmixed_base + APMIXED_SYS_TS_CON1);
+	tmp &= ~(0x37);
+	tmp |= 0x1;
+	writel(tmp, apmixed_base + APMIXED_SYS_TS_CON1);
 	udelay(200);
 }
 
@@ -1194,10 +1184,10 @@ static int mtk_thermal_probe(struct platform_device *pdev)
 		goto err_disable_clk_auxadc;
 	}
 
-	mtk_thermal_turn_on_buffer(mt, apmixed_base);
-
-	if (mt->conf->version != MTK_THERMAL_V2)
+	if (mt->conf->version != MTK_THERMAL_V1) {
+		mtk_thermal_turn_on_buffer(apmixed_base);
 		mtk_thermal_release_periodic_ts(mt, auxadc_base);
+	}
 
 	if (mt->conf->version == MTK_THERMAL_V1)
 		mt->raw_to_mcelsius = raw_to_mcelsius_v1;
